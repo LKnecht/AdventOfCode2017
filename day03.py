952751ea0b83b46aaa7636858cccb4bcecb28098
Lucas Weinknecht <kuddelmuddel@ok.de>
2017-12-03 16:35:40 +0100

    Readability

--- day03.py
+++ day03.py
@@ -5,30 +5,30 @@
 #  20   7   8   9  10
 #  21  22  23---> ...
 
-def layerSize(l):
-    return 8*(l-1) if l > 1 else 1
+def layerSize(layer):
+    return 8*(layer-1) if layer > 1 else 1
 
 def layerIndex(n):
-    s, l = 0, 1
-    while s < n:
-        s += layerSize(l)
-        l += 1
-    return l - 1
+    sizeSum, layer = 0, 1
+    while sizeSum < n:
+        sizeSum += layerSize(layer)
+        layer += 1
+    return layer - 1
 
-def lastNumberInLayer(l):
-    return (l - 1) * l * 4 + 1  # variation of gauss sum formula
+def lastNumberInLayer(layer):
+    return (layer - 1) * layer * 4 + 1  # variation of gauss sum formula
 
-def distToQuadrantMiddle(n, l):
-    quadSize = layerSize(l) // 4
-    lln = lastNumberInLayer(l)
-    while n < lln:
-        lln -= quadSize
-    return abs(quadSize // 2 + lln - n)
+def distToQuadrantMiddle(n, layer):
+    quadSize = layerSize(layer) // 4
+    lastNumInQuad = lastNumberInLayer(layer)
+    while n < lastNumInQuad:
+        lastNumInQuad -= quadSize
+    return abs(quadSize // 2 + lastNumInQuad - n)
 
 def ManhattenDistance(n):
     if n == 1: return 0
-    l = layerIndex(n)
-    return l + distToQuadrantMiddle(n, l) - 1
+    layer = layerIndex(n)
+    return layer + distToQuadrantMiddle(n, layer) - 1
 
 assert(ManhattenDistance(1) == 0)
 assert(ManhattenDistance(12) == 3)
